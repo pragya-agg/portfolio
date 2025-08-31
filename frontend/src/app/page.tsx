9f844daade3196ab9578180322a7f04fc04205b6
anandaa-arch <anandraj2023.ainds@mmcoe.edu.in>
2025-08-31 16:46:20 +0530

    Trigger deployment - theme fixes should be live

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,8 @@ import { FeaturedProjects } from '@/components/featured-projects'
 import { TechStack } from '@/components/tech-stack'
 import { CallToAction } from '@/components/call-to-action'
 
+// Updated: Theme toggle functionality fixed
+
 export default function Home() {
   return (
     <>
